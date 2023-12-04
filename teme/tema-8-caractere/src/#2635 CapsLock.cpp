0c5373d93790c4bcd9c67c715b91ef5988f6fcb2
Valyn <115489989+ValynTyler@users.noreply.github.com>
2023-12-04 22:43:04 +0200

    Tema 8

--- teme/tema-8-caractere/src/#2635 CapsLock.cpp
+++ teme/tema-8-caractere/src/#2635 CapsLock.cpp
@@ -7,9 +7,24 @@ ifstream fin("capslock.in");
 ofstream fout("capslock.out");
 
 int main() {
-    char a;
-    fin >> a;
-    fout << a;
-    cout << a;
+
+    bool caps_on = false;
+    char c = fin.get();
+
+    while (c != -1 && c != '\n') {
+        if (c == '#') {
+            caps_on = !caps_on;
+        }
+        else {
+            if (caps_on && c >= 'a' && c <= 'z') {
+                c += 'A' - 'a';
+            }
+
+            fout << c;
+        }
+
+        c = fin.get();
+    }
+
     return 0;
 }
