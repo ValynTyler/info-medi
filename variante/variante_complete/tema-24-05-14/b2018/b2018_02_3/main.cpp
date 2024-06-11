cc9466e552796d4fe5f2465ce7fca2c9af39b8e5
Valyn <115489989+ValynTyler@users.noreply.github.com>
2024-06-11 15:20:30 +0300

    b2018_02_3_4

--- variante/variante_complete/tema-24-05-14/b2018/b2018_02_3/main.cpp
+++ variante/variante_complete/tema-24-05-14/b2018/b2018_02_3/main.cpp
@@ -1,9 +1,60 @@
 #include <iostream>
 
 using namespace std;
+/*
+Subiectul al III-lea
+--------------------
+1.
+{
+    PHP     = 1,
+    Java    = 2,
+    Python  = 3,
+    C#      = 4,
+    SQL     = 5,
+}
+
+Reguli:
+ - nr PAR (nenul) de limbaje
+ - 2 EXCLUDE 3
+ - contează ORDINEA
+ - un limbaj apare O DATĂ
+
+1: (1, 2)
+2: (1, 2, 4, 5)
+3: (1, 2, 5, 4)
+4: (1, 3)
+5: (1, 3, 4, 5)
+.
+.
+.
+n: (2, 1, 5, 4)
+n+1: ?
+n+1: (2, 1, 5, x) NU
+n+1: (2, 1, x, x) NU
+n+1: (2, 2, x, x) NU
+n+1: (2, 3, x, x) NU (reg II)
+n+1: (2, 4) !!! => a.
+
+2. orice mai mare ca 5?
+Răspunsuri: 42, 43, 44, ...
+
+3.
+(Mai jos)
+*/
+
+int resturi(int n, int x, int y, int r) {
+    int output = 0;
+    for (int i = 0; i < n; i++) {
+        if (i % x == r && i % y == r) {
+            output += 1;
+        }
+    }
+    return output;
+}
 
 int main() {
-    cout << "Hello from InfRO!";
-  
+
+    // cout << resturi(200, 5, 14, 2);
+
     return 0;
 }
--- variante/variante_complete/tema-24-05-14/b2018/b2018_02_3_4/main.cpp
+++ variante/variante_complete/tema-24-05-14/b2018/b2018_02_3_4/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+ifstream fin("bac.txt");
+
 int main() {
-    cout << "Hello from InfRO!";
-  
+    int len_max = 0;
+    int prv, crt, nxt;
+
+    fin >> prv >> crt >> nxt;
+    
+    int len = 2;
+
+    do {
+        if (prv != crt && crt != nxt) {
+            len += 1;
+        } else {
+            if (len > len_max) {
+                len_max = len;
+            }
+            len = 2;
+        }
+
+        prv = crt;
+        crt = nxt;
+    } while (fin >> nxt);
+
+    cout << len_max;
+
     return 0;
 }
